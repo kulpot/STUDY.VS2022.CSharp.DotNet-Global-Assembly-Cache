ec8c7099a34e1ba13a2afcd84897adc875c2fe44
Kulpot <kulpotwahu@gmail.com>
2023-09-28 10:52:45 +0800

    Add project files.

---
+++ DotNet Global Assembly Cache/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=3Fr2AgycXeI&list=PLRwVmtr-pp06rfSgNYu_oBg40DkwXiRHt&index=19
+// GAC - GLOBAL Assembly Cache
+
+class MainClass
+{
+    static void Main()
+    {
+
+    }
+}
